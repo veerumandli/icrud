3c227f5dca85793dee57e52cee56ebcd8ca01c48
veerumandli <veeru.mandli@gmail.com>
2025-10-22 15:06:56 +0530

    Update prod.py

--- prod.py
+++ prod.py
@@ -1,7 +1,7 @@
 from src.index import create_app
 
 # -------------------------
-# APP RUNNER
+# APP RUNNER for prduction
 # -------------------------
 if __name__ == '__main__':
     create_app().run(debug=False, host='0.0.0.0', port=5002)
