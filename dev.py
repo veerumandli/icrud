40fd169e75a127b30b6d838625ff5efcb23026ae
veerumandli <veeru.mandli@gmail.com>
2025-10-24 14:52:11 +0530

    Update dev.py

--- dev.py
+++ dev.py
@@ -1,7 +1,7 @@
 from src.index import create_app
 
 # -------------------------
-# APP RUNNER for devlopment environment
+# APP RUNNER for devlopment
 # -------------------------
 if __name__ == '__main__':
     create_app().run(debug=True)
